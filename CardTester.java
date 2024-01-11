9d9838420e8664075e0507a32da3092f8af91b34
yuzun25 <4604490-yuzun25@users.noreply.replit.com>
2024-01-11 16:18:55 +0000

    Initial commit

---
+++ CardTester.java
@@ -0,0 +1,17 @@
+/**
+ * This is a class that tests the Card class.
+ */
+public class CardTester {
+
+	/**
+	 * The main method in this class checks the Card operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		Card kazimSamed = new Card("As", "Karo", 5);
+    Card mertKurar = new Card("2", "Sivrisinek", 5);
+    Card yigitUzun = new Card("3", "Diamonds", 5);
+
+    yigitUzun.toString();
+	}
+}
